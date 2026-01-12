c27bf8b1e3d3a8a45f9325b0af6c29effa8309d6
iqonese <iqonese@gmail.com>
2026-01-12 13:05:23 +0900

    Remove junk from repo

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+let weatherTable: UITableView()
 
 class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        weatherTable.dataSource = self
     }
+    
+    
 }
+
+
+extension weatherTable: UITableViewDataSource, UITableViewDelegate {
+    
+}
+
+
 
